10ae980b6903da81ab5174681ccf90b8cf1910f1
rythae <auxano42@gmail.com>
2019-03-07 14:59:45 +0100

    Updates

---
+++ precache-manifest.053b7f7e45426b01b45b560406d6d53d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dbe8c55bb9fe56be759409d439199d9",
+    "url": "/music-lyrics/static/media/music.8dbe8c55.jpg"
+  },
+  {
+    "revision": "6b8d5405d07c400b98bc",
+    "url": "/music-lyrics/static/js/runtime~main.6b8d5405.js"
+  },
+  {
+    "revision": "8aea648d47a0510bb76a",
+    "url": "/music-lyrics/static/js/main.8aea648d.chunk.js"
+  },
+  {
+    "revision": "f5b96aec2300aca7d657",
+    "url": "/music-lyrics/static/js/2.f5b96aec.chunk.js"
+  },
+  {
+    "revision": "8aea648d47a0510bb76a",
+    "url": "/music-lyrics/static/css/main.823f0aed.chunk.css"
+  },
+  {
+    "revision": "c59fb4bd4b8fa2ef594cb6f4e8b7564d",
+    "url": "/music-lyrics/index.html"
+  }
+];
